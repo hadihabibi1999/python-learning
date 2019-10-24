a959448b9b6d105150d0c265f3694979ec7a086e
HadiCyb <hadihabibi2000@gmail.com>
2019-10-24 15:05:57 +0330

    remove duplicate in a list

--- app.py
+++ app.py
@@ -1,27 +1,11 @@
 # python
+# write program to remove the duplicates in a list
 
-started = False
-while True:
- command = input('> ').lower()
- if command == 'start':
-        if not started:
-            print('car started ...')
-            started = True
-        else:
-            print('car already started !!')
- elif command == 'stop':
-        if not started:
-            print('car already stopped!!')
-        else:
-            print('car stopped')
-            started = False
- elif command == 'help':
-        print('''
- start -> start the car 
- stop -> stop the car 
- quit -> to quit
-               ''')
- elif command == 'quit':
-        break
- else:
-  print('dont understand that !!')
+numbers = [1, 3, 5, 6, 6, 8, 1, 1, 4, 4, 4, 7, 7]
+ 
+for number in numbers:
+    counting = numbers.count(number)
+    if counting > 1:
+        numbers.remove(number)
+
+print(numbers)
