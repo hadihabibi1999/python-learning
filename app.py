c44f7bee823ae2ba5b37e86e34a45519f04e8edc
HadiCyb <hadihabibi2000@gmail.com>
2019-10-24 15:48:57 +0330

    dictionary

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # python
-# write program to remove the duplicates in a list
 
-numbers = [1, 3, 5, 6, 6, 8, 1, 1, 4, 4, 4, 7, 7]
- 
-for number in numbers:
-    counting = numbers.count(number)
-    if counting > 1:
-        numbers.remove(number)
-
-print(numbers)
+dictionary = {
+    "1": 'one',
+    "2": 'two',
+    "3": 'three',
+    "4": 'four',
+    "5": 'five',
+    "6": 'six',
+    "7": 'seven',
+    "8": 'eight',
+    "9": 'nine',
+}
+phone = input('>phone: ')
+out = ''
+for num in phone:
+    out += dictionary.get(num)+', '
+print(f'>phone => {out}')
